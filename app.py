2dbd2d2ad3b90764886786508f9432d4f0a720b9
280051hubert <280051@student.pwr.edu.pl>
2026-06-10 17:07:02 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+redis_url = os.environ.get("REDIS_URL", "redis://redis:6379")
+cache = redis.from_url(redis_url)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr("hits")
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route("/")
+def hello():
+    count = get_hit_count()
+    return f"<h1>site visited {count} times</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
